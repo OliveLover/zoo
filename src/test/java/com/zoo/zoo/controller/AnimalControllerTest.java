3424c12aa0f264eac0ff0150bc965295baaca8ba
tiktokyi <tiktokyi@gmail.com>
2023-07-26 09:26:23 +0900

    Test : AnmialController Test코드 Mock단위 테스트로 작성해보기

--- src/test/java/com/zoo/zoo/controller/AnimalControllerTest.java
+++ src/test/java/com/zoo/zoo/controller/AnimalControllerTest.java
@@ -1,58 +1,59 @@
 package com.zoo.zoo.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoo.zoo.model.dto.AdoptRequestDto;
-import com.zoo.zoo.model.entity.AnimalType;
 import com.zoo.zoo.service.AnimalService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static com.zoo.zoo.model.entity.AnimalType.FOX;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class AnimalControllerTest {
-    @Autowired
     MockMvc mockMvc;
 
-    @Autowired
+    @Mock
     ObjectMapper objectMapper;
 
-    @MockBean
+    @Mock
     AnimalService animalService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new AnimalController(animalService)).build();
+    }
 
     @Test
     void adoptAnAnimal() throws Exception {
         // given
         AdoptRequestDto requestDto = new AdoptRequestDto(FOX, "미호", "female");
+        String answer = "success";
 
-        given(animalService.adoptAnAnimal(any())).willReturn(new ResponseEntity<>("입양되었습니다.", HttpStatus.OK));
+        when(animalService.adoptAnAnimal(any())).thenReturn(new ResponseEntity<>(answer, HttpStatus.OK));
         // when & then
-        mockMvc.perform(post("/animal")
+        mockMvc.perform(MockMvcRequestBuilders.post("/animal")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(requestDto)))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("입양되었습니다."))
+                .andExpect(MockMvcResultMatchers.content().string(answer))
                 .andDo(print());
     }
 }
